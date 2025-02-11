d6da78f4d5cdfd0d56ba5bfdfe48163a5c3ab0a0
Afham <111404292+auth-Afham@users.noreply.github.com>
2025-02-11 08:32:28 +0800

    initialize Vite + React + TypeScript project with basic structure and components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <p>Welcome to Quantum Learning!</p>
+    </div>
+  );
+};
+
+export default Home;
